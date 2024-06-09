a871e033e14222009830d66c6c15d589b3c51d5d
Maruqes <m4rques1s0n@gmail.com>
2024-06-10 00:03:15 +0100

    better performance :D

--- check_files.js
+++ check_files.js
@@ -7,100 +7,93 @@ const print = require("./extras.js")
 
 const userHomeDir = os.homedir()
 
-function get_file_hash_path(file_path)
-{
+function get_file_hash_path(file_path) {
     const file_buffer = fs.readFileSync(file_path);
     const hash = crypto.createHash('sha256');
     hash.update(file_buffer);
     return hash.digest('hex');
 }
 
-function get_file_hash_buf(buf)
-{
+function get_file_hash_buf(buf) {
     const hash = crypto.createHash('sha256');
     hash.update(buf);
     return hash.digest('hex');
 }
 
-function is_file_changed(file_buffer1, file_hash)
-{
+function is_file_changed(file_buffer1, file_hash) {
 
     //create an hash of the file
     let hash1 = get_file_hash_buf(file_buffer1)
 
-    if (hash1 !== file_hash)
-    {
+    if (hash1 !== file_hash) {
         return true;
     }
     return false;
 }
 
-async function compare_folder_to_db_delete_files()
-{
-    const files = await db.get_all_files();
+async function compare_folder_to_db_delete_files(db_files) {
 
-    files.forEach(file =>
-    {
-        if (!fs.existsSync(file.file_path))
-        {
-            db.delete_file(file.file_path)
+    for (let i = 0; i < db_files.length; i++) {
+        const file_db = db_files[i];
+        const file_path = file_db.file_path;
+        if (!fs.existsSync(file_path)) {
+            db.delete_file(file_path)
         }
-    });
+    }
 }
 
 
 
-async function update_hash_and_add_to_table(file_path)
-{
+async function update_hash_and_add_to_table(file_path, db_files) {
     const file_buffer = fs.readFileSync(file_path);
 
-    const file_db = await db.get_file(file_path)
-    if (file_db == null)
-    {
+    const file_db = db_files.find(file => file.file_path === file_path);
+
+    if (file_db == null) {
         const hash = get_file_hash_buf(file_buffer)
         db.insert_file(file_path, hash)
     }
-    else
-    {
+    else {
         const hash = get_file_hash_buf(file_buffer)
-        if (is_file_changed(file_buffer, file_db.file_hash))
-        {
+        if (is_file_changed(file_buffer, file_db.file_hash)) {
             db.update_file(file_path, hash)
         }
     }
+
 }
 
+async function compare_folder_to_db_files(dir_path, folder, db) {
 
-async function get_all_hashes(dir_path)
-{
-    //get all files in the folder and subfolders
-    const files_and_folders = fs.readdirSync(dir_path);
-
-    await files_and_folders.forEach(file =>
-    {
+    folder.forEach(file => {
         const filePath = `${dir_path}/${file}`;
         const stats = fs.statSync(filePath);
-        if (stats.isFile())
-        {
+        if (stats.isFile()) {
             if (!filePath.includes(".cloud_storage"))
-                update_hash_and_add_to_table(filePath);
-        } else if (stats.isDirectory())
-        {
-            if (!filePath.includes("."))
-            {
-                get_all_hashes(filePath)
+                update_hash_and_add_to_table(filePath, db);
+        } else if (stats.isDirectory()) {
+            if (!filePath.includes(".")) {
+                folder = fs.readdirSync(filePath);
+                compare_folder_to_db_files(filePath, folder, db)
             }
         }
     });
 
-    compare_folder_to_db_delete_files();
+}
+
+
+async function get_all_hashes(dir_path) {
+    //get all files in the folder and subfolders
+    const files_and_folders = fs.readdirSync(dir_path);
+    const db_files = await db.get_all_files();
+
+    compare_folder_to_db_files(dir_path, files_and_folders, db_files)
+    compare_folder_to_db_delete_files(db_files);
 }
 
 
 
-async function testes()
-{
-    await get_all_hashes(userHomeDir + "/cloud_storage")
+async function testes() {
+    await get_all_hashes(userHomeDir + "/cloud_storage", null)
 }
 
 
