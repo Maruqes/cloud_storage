7bbe0b3c360dcd007ac4d2de9f526b99fe34b9e3
Maruqes <m4rques1s0n@gmail.com>
2024-06-16 17:49:19 +0100

    add

--- db.js
+++ db.js
@@ -8,24 +8,18 @@ const userHomeDir = os.homedir();
 
 let db = new sqlite3.Database(userHomeDir + "/cloud_storage/.cloud_storage.db");
 
-async function create_table()
-{
+async function create_table() {
     await db.run("CREATE TABLE IF NOT EXISTS files (file_path TEXT PRIMARY KEY, file_hash TEXT)");
     await db.run("CREATE TABLE IF NOT EXISTS last_sync (id INTEGER PRIMARY KEY AUTOINCREMENT, last_sync TEXT)")
     print.okay("Created table files");
 }
 
 
-async function insert_file(file_path, file_hash)
-{
-    return await new Promise(async (resolve, reject) =>
-    {
-        db.serialize(async function ()
-        {
-            await db.run(`INSERT INTO files (file_path, file_hash) VALUES ("${file_path}", "${file_hash}")`, (err) =>
-            {
-                if (err)
-                {
+async function insert_file(file_path, file_hash) {
+    return await new Promise(async (resolve, reject) => {
+        db.serialize(async function () {
+            await db.run(`INSERT INTO files (file_path, file_hash) VALUES ("${file_path}", "${file_hash}")`, (err) => {
+                if (err) {
                     reject(err);
                 }
                 print.okay(`Inserted file: ${file_path}`);
@@ -35,16 +29,11 @@ async function insert_file(file_path, file_hash)
     })
 }
 
-async function update_file(file_path, file_hash)
-{
-    return await new Promise(async (resolve, reject) =>
-    {
-        db.serialize(async function ()
-        {
-            await db.run(`UPDATE files SET file_hash = "${file_hash}" WHERE file_path = "${file_path}"`, (err) =>
-            {
-                if (err)
-                {
+async function update_file(file_path, file_hash) {
+    return await new Promise(async (resolve, reject) => {
+        db.serialize(async function () {
+            await db.run(`UPDATE files SET file_hash = "${file_hash}" WHERE file_path = "${file_path}"`, (err) => {
+                if (err) {
                     reject(err);
                 }
                 print.okay(`Updated file: ${file_path}`);
@@ -54,16 +43,11 @@ async function update_file(file_path, file_hash)
     });
 }
 
-async function delete_file(file_path)
-{
-    db.serialize(async function ()
-    {
-        return await new Promise(async (resolve, reject) =>
-        {
-            await db.run(`DELETE FROM files WHERE file_path = "${file_path}"`, (err) =>
-            {
-                if (err)
-                {
+async function delete_file(file_path) {
+    db.serialize(async function () {
+        return await new Promise(async (resolve, reject) => {
+            await db.run(`DELETE FROM files WHERE file_path = "${file_path}"`, (err) => {
+                if (err) {
                     reject(err);
                 }
                 print.okay(`Deleted file: ${file_path}`);
@@ -73,18 +57,13 @@ async function delete_file(file_path)
     })
 }
 
-async function get_all_files()
-{
+async function get_all_files() {
     print.info("Getting all files from database");
 
-    return await new Promise(async (resolve, reject) =>
-    {
-        db.serialize(async function ()
-        {
-            await db.all("SELECT * FROM files", (err, rows) =>
-            {
-                if (err)
-                {
+    return await new Promise(async (resolve, reject) => {
+        db.serialize(async function () {
+            await db.all("SELECT * FROM files", (err, rows) => {
+                if (err) {
                     reject(err);
                 }
                 resolve(rows);
@@ -93,16 +72,11 @@ async function get_all_files()
     });
 }
 
-async function get_file(file_path)
-{
-    return await new Promise(async (resolve, reject) =>
-    {
-        db.serialize(async function ()
-        {
-            await db.get(`SELECT * FROM files WHERE file_path = "${file_path}"`, (err, row) =>
-            {
-                if (err)
-                {
+async function get_file(file_path) {
+    return await new Promise(async (resolve, reject) => {
+        db.serialize(async function () {
+            await db.get(`SELECT * FROM files WHERE file_path = "${file_path}"`, (err, row) => {
+                if (err) {
                     reject(err);
                 }
                 resolve(row);
@@ -112,13 +86,10 @@ async function get_file(file_path)
 }
 
 
-async function update_sync_db()
-{
-    return await new Promise(async (resolve, reject) =>
-    {
+async function update_sync_db() {
+    return await new Promise(async (resolve, reject) => {
         const date = new Date();
-        db.serialize(async function ()
-        {
+        db.serialize(async function () {
             await db.run(`INSERT INTO last_sync (last_sync) VALUES ("${date}")`);
             print.okay(`Updated last sync: ${date}`)
 
@@ -129,21 +100,60 @@ async function update_sync_db()
 }
 
 
-async function get_last_sync()
-{
-    return await new Promise(async (resolve, reject) =>
-    {
-        db.serialize(async function ()
-        {
-            await db.get(`SELECT * FROM last_sync ORDER BY id DESC LIMIT 1`, (err, row) =>
-            {
-                if (err)
-                {
+async function get_last_sync() {
+    return await new Promise(async (resolve, reject) => {
+        db.serialize(async function () {
+            await db.get(`SELECT * FROM last_sync ORDER BY id DESC LIMIT 1`, (err, row) => {
+                if (err) {
                     reject(err);
                 }
                 resolve(row);
             });
         });
+    });
+}
+
+
+async function close_db() {
+    await db.close();
+}
+
+async function open_db() {
+    db = await new sqlite3.Database(userHomeDir + "/cloud_storage/.cloud_storage.db");
+}
+
+
+async function open_temp_db() {
+    return await new sqlite3.Database(userHomeDir + "/cloud_storage/.cloud_storage_temp.db");
+}
+
+async function close_temp_db(db_temp) {
+    await db_temp.close();
+}
+
+async function get_last_sync_temp_db(db_temp) {
+    return await new Promise(async (resolve, reject) => {
+        db_temp.serialize(async function () {
+            await db_temp.all("SELECT * FROM last_sync ORDER BY id DESC LIMIT 1", (err, rows) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(rows);
+            });
+        })
+    });
+}
+
+async function get_all_files_temp_db(db_temp) {
+    return await new Promise(async (resolve, reject) => {
+        db_temp.serialize(async function () {
+            await db_temp.all("SELECT * FROM files", (err, rows) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(rows);
+            });
+        })
     });
 }
 
@@ -157,4 +167,10 @@ module.exports = {
     get_file,
     update_sync_db,
     get_last_sync,
+    close_db,
+    open_db,
+    open_temp_db,
+    close_temp_db,
+    get_last_sync_temp_db,
+    get_all_files_temp_db,
 };
